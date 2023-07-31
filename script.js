2e896abe0316c5cc27567d6ae982e028dc21899c
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-07-31 19:11:38 -0400

    Added form Validation and fetching LocalStorage data

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name')
 const websiteUrlEl = document.getElementById('website-url')
 const bookmarksContainer = document.getElementById('bookmarks-container')
 
+let bookmarks = []
+
 // Show Modal, Focus on Input
 function showModal() {
   modal.classList.toggle('show-modal')
@@ -16,3 +18,69 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false))
+
+// Validate Form
+function validate(nameValue, urlValue) {
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression)
+  if(!nameValue || !urlValue) {
+    alert('Please submit values for both fields')
+    return false
+  }
+  if (urlValue.match(regex)) {
+    alert('match')
+  }
+  if(!urlValue.match(regex)) {
+    alert('Please provide a valid web address')
+    return false;
+  }  
+// Valid
+return true
+}
+
+// Fetch Boomarks
+function fetchBookmarks() {
+  // Get bookmarks from LocalStorage if available
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+  } else {
+    // Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: 'Mubi',
+        url: 'https://mubi.com',
+      }
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+  }
+  console.log(bookmarks)
+}
+
+// Handle Data from Form
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value
+  let urlValue = websiteUrlEl.value
+  if (!urlValue.includes('http://', "https://")) {
+    urlValue = `https://${urlValue}`
+  }
+  if(!validate(nameValue, urlValue)) {
+    return false;
+  }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  }
+  bookmarks.push(bookmark)
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+  fetchBookmarks()
+  bookmarkForm.reset()
+  websiteNameEl.focus()
+}
+
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark)
+
+// On Load, Fetch Bookmarks
+fetchBookmarks();
